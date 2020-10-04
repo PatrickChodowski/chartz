79bfcb756a3f91fddc42a1817930060743315d41
patrick <chodowski.patrick@gmail.com>
2020-10-04 23:18:16 +0200

    fix readme

--- setup.py
+++ setup.py
@@ -11,5 +11,5 @@ setup(
     version='0.1',
     license='MIT',
     description='Flask module allowing to build dashboards for data defined in single yml files',
-    long_description=open('README.txt').read(),
+    long_description=open('README.rst').read(),
 )

a35928db503366de4dc4c8ac13c867a34ba4757d
patrick <chodowski.patrick@gmail.com>
2020-10-09 20:56:01 +0200

    changed license

--- setup.py
+++ setup.py
@@ -14,10 +14,10 @@ setup(
                               'chartz_static/bootstrap/css/*.map',
                               'chartz_static/bootstrap/js/*.js',
                               'chartz_static/bootstrap/js/*.map']},
-    install_requires=['flask', 'bokeh', 'google-cloud-bigquery','google-cloud-storage' ,'pandas', 'pyyaml', 'sqlalchemy'],
+    install_requires=['flask', 'bokeh', 'google-cloud-bigquery', 'google-cloud-storage','pandas', 'pyyaml', 'sqlalchemy'],
     # *strongly* suggested for sharing
     version='0.4.9',
-    license='MIT',
+    license='GPLv3',
     description='Flask module allowing to build dashboards for data defined in single yml files',
     long_description=open('README.md').read(),
 )

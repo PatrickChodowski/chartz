927e6d2e72200c583273d2e5b49a61a6b2d382e2
patrick <chodowski.patrick@chalhoub.com>
2020-11-29 09:53:29 +0400

    added function

--- chartz/__init__.py
+++ chartz/__init__.py
@@ -1,3 +1,3 @@
 from chartz.chartz_class import chartz
-from chartz.utils import setup_env
+from chartz.utils import setup_env, create_filters, make_table_yml
 from chartz.sql_builder import SqlBuilder
